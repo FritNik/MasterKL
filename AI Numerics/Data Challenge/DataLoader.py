d4c55ac7ef3b3ca36a20e29bc91678111ebf1d90
Felix <felix.fritz02@gmx.de>
2025-05-07 17:04:06 +0200

    s

---
+++ AI Numerics/Data Challenge/DataLoader.py
@@ -0,0 +1,19 @@
+from ucimlrepo import fetch_ucirepo
+
+class HeartFailureDataset:
+    def __init__(self):
+        # Dataset abrufen
+        heart_failure_clinical_records = fetch_ucirepo(id=519)
+        
+        # Daten als Attribute speichern
+        self.X = heart_failure_clinical_records.data.features
+        self.y = heart_failure_clinical_records.data.targets
+
+    def get_features(self):
+        """Gibt die Features zurück."""
+        return self.X
+
+    def get_targets(self):
+        """Gibt die Zielvariable zurück."""
+        return self.y
+  
